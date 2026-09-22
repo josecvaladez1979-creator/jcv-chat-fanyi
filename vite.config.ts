cbca7842223977a589c2ff5e1d3b0dcf76dce2dc
josecvaladez1979-creator <josecvaladez1979@gmail.com>
2026-09-21 21:02:37 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración avanzada para compilar JCV CHAT FĀNYÌ en la infraestructura gratuita de GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: './' // Asegura que los enlaces de estilos y lógica funcionen en cualquier celular
+  base: '/jcv-chat-fānyì/' // Reemplaza la ruta genérica por el nombre exacto de tu repositorio de GitHub
 })
